c24cd70a93b9af51f74ff648918c346128d0471a
Ceyda <120778327+CeyRc@users.noreply.github.com>
2025-06-08 12:34:27 +0000

    show main menu methodu ekledim

--- interface1.py
+++ interface1.py
@@ -28,3 +28,30 @@ class SpellCorrectorApp:
                                     font=("Arial", 22, "bold"), bg="#f8f8f6", fg="#5222aa")
 
         self.animate_logo()
+
+
+
+
+
+        def show_main_menu(self):
+        word_icon = ImageTk.PhotoImage(Image.open("word_icon.jpg").resize((100, 100)))
+        self.word_button = tk.Button(self.root, image=word_icon,
+                                     command=self.open_word_screen, bg="#f8f8f6", bd=0,
+                                     highlightthickness=0, relief="flat", activebackground="#f8f8f6")
+        self.word_button.image = word_icon
+        self.word_button.place(relx=0.3, rely=0.60, anchor="center")
+
+        self.word_label = tk.Label(self.root, text="For Word Correction",
+                                   font=("Arial", 15), bg="#f8f8f6", fg="#444444")
+        self.word_label.place(relx=0.3, rely=0.72, anchor="center")
+
+        sentence_icon = ImageTk.PhotoImage(Image.open("sentence_icon.png").resize((100, 100)))
+        self.sentence_button = tk.Button(self.root, image=sentence_icon,
+                                         command=self.open_sentence_screen, bg="#f8f8f6", bd=0,
+                                         highlightthickness=0, relief="flat", activebackground="#f8f8f6")
+        self.sentence_button.image = sentence_icon
+        self.sentence_button.place(relx=0.7, rely=0.60, anchor="center")
+
+        self.sentence_label = tk.Label(self.root, text="For Sentence Correction",
+                                       font=("Arial", 15), bg="#f8f8f6", fg="#444444")
+        self.sentence_label.place(relx=0.7, rely=0.72, anchor="center")
